cfef18a8e1c6fd28c3f9a351b037b6f6c847fc49
Jerry <59536381+Jerryc95@users.noreply.github.com>
2023-06-15 13:36:28 -0400

    add featured project and mobile nav

--- src/components/featuredProject.tsx
+++ src/components/featuredProject.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./featuredProject.css";
+import NexusGameExample from "./../assets/NexusGameExample.png";
 
 const FeaturedProject = () => {
   const stack = [
@@ -14,27 +15,40 @@ const FeaturedProject = () => {
   return (
     <div className="featured-project-container">
       <div className="featured-project-title">Featured Project</div>
-      <div className="feature-project-name">
-        Nexus: Magic the gathering Companion
-      </div>
-      {/* image here  */}
+      <a
+        className="project-link"
+        href={"https://playnexus.app"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nexus: MTG Companion
+      </a>
       <div className="featured-project-details">
-        <p className="featured-project-about">
-          Nexus is a mobile and web app designed for Magic: The Gathering
-          players who want to easily manage their health during games. With
-          Nexus, players can keep track and of their own life total and the life
-          totals of their opponents in real time, making it easier to stay
-          focused on the game without worrying about constantly looking at
-          everyone's dice or an app across the table.
-        </p>
-        <div className="project-stack">Tech used:</div>
-        <ul>
-          {stack.map((tech, id) => (
-            <li className="feature-project-stack-item" key={id}>
-              {tech}
-            </li>
-          ))}
-        </ul>
+        <img
+          className="featured-project-image"
+          src={NexusGameExample}
+          alt="Nexus Gameplay"
+        />
+        <div className="featured-project-about">
+          <p>
+            Nexus is an iOS and web app designed for Magic: The Gathering players
+            who want to easily manage their health during games. With Nexus,
+            players can keep track and of their own life total and the life
+            totals of their opponents in real time, making it easier to stay
+            focused on the game without worrying about constantly looking at
+            everyone's dice or an app across the table.
+          </p>
+          <p>Technology Stack:</p>
+          <div className="project-stack">
+            <ul>
+              {stack.map((tech, id) => (
+                <li className="project-stack-item" key={id}>
+                  {tech}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );

d4cf7721c5f6686275dff7c92a794000b82bd68b
Jerry <59536381+Jerryc95@users.noreply.github.com>
2024-02-11 19:28:30 -0500

    new commit

--- src/assets/appItems.ts
+++
@@ -1,5 +0,0 @@
-import { AppItem } from "../models/AppItem";
-import { BatteryVision } from "./apps/batteryVision";
-import { TunnelDrop } from "./apps/tunnelDrop";
-
-export const appItems: AppItem[] = [BatteryVision];
